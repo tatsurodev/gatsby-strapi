41a0655799ea61894d3b7550ee4f352bee174f0a
Tatsuro <contacts@tatsuro.dev>
2021-01-16 17:07:10 +0900

    gatsbyにreact-bootstrap, bootswatch install

---
+++ frontend/src/components/common/Button.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Button as BsButton } from "react-bootstrap"
+import "bootswatch/dist/litera/bootstrap.min.css"
+
+const Button = () => {
+  return <BsButton variant="danger">button</BsButton>
+}
+
+export { Button }
