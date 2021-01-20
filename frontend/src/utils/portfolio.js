4a65b63b5e67c8906d4f540511aee7896b4a7506
Tatsuro <contacts@tatsuro.dev>
2021-01-21 05:39:30 +0900

    websitesから重複のないtagsを取得するutilsを作成

---
+++ frontend/src/utils/portfolio.js
@@ -0,0 +1,23 @@
+function getNoDuplicateTags(websites) {
+  const tagsArray = websites.map(website => website.tags.map(tag => tag.name))
+  // console.log("tagsArray", tagsArray)
+  // 重複を排除したset objectを作成
+  let tagsSet = new Set()
+  tagsArray.map(tagArray =>
+    tagArray.map(tag => {
+      // nullでなければ追加
+      if (tag) {
+        tagsSet.add(tag)
+      }
+    })
+  )
+  // console.log("tagsArray", tagsArray)
+  // setからarrayへ変換
+  const tags = Array.from(tagsSet)
+  // spread operatorで変換してもおｋ
+  // const tags = [...tagSet]
+  // console.log("tags", tags)
+  return tags
+}
+
+export { getNoDuplicateTags }
