615f64d3bac67c7c82b774dc0fda3eba77c8b78e
Tatsuro <contacts@tatsuro.dev>
2021-01-21 15:40:57 +0900

    Reviews, Review作成

---
+++ frontend/src/components/Reviews.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import { useLocation } from "@reach/router"
+import Review from "./Review"
+import { Title, TypeButtons } from "./common"
+
+const Reviews = ({ reviews: reviewsProp, title }) => {
+  const [reviews, setReviews] = useState(reviewsProp)
+  const setBackToAll = () => {
+    setReviews(reviewsProp)
+  }
+  const { pathname } = useLocation()
+
+  return (
+    <section>
+      <Title title={title} />
+      {pathname === "/reviews" && (
+        <TypeButtons
+          reviews={reviewsProp}
+          setReviews={setReviews}
+          setBackToAll={setBackToAll}
+        />
+      )}
+      <div className="row">
+        {reviews.map((review, index) => {
+          return <Review key={review.id} index={index} {...review} />
+        })}
+      </div>
+      {pathname === "/" && (
+        <div className="text-center mt-3">
+          <Link to="/reviews" className="btn btn-info">
+            Check Other Reviews
+          </Link>
+        </div>
+      )}
+    </section>
+  )
+}
+
+export default Reviews
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,16 +2,22 @@ import React from "react"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Portfolios from "../components/Portfolios"
+import Reviews from "../components/Reviews"
 
 const IndexPage = ({ data }) => {
   const {
     allStrapiPortfolios: { nodes: portfolios },
+    allStrapiReviews: { nodes: reviews },
   } = data
 
   return (
     <Layout>
       <Hero />
       <Portfolios portfolios={portfolios} title="Featured Portfolios" />
+      <Reviews
+        reviews={reviews}
+        title="Featured Reviews of Udemy Courses and Books"
+      />
     </Layout>
   )
 }
@@ -40,6 +46,42 @@ export const query = graphql`
             id
             name
           }
+        }
+      }
+    }
+    allStrapiReviews(
+      filter: { featured: { eq: true } }
+      limit: 4
+      sort: { fields: date, order: DESC }
+    ) {
+      nodes {
+        id
+        type
+        title
+        author
+        url
+        review
+        rating
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        slug
+        date(formatString: "YYYY, MM")
+        websites {
+          id
+          title
+          description
+          github
+          url
+        }
+        tags {
+          id
+          name
+          slug
         }
       }
     }
