b53bbf706bd679d5211380a2f8bd818baccd24e0
Tatsuro <contacts@tatsuro.dev>
2021-01-23 14:40:57 +0900

    validationに必要なtouched, validated, errors, loading stateをhooksに追加、submit時のlogic完成

--- frontend/src/hooks/useContactValidation.js
+++ frontend/src/hooks/useContactValidation.js
@@ -1,24 +1,108 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import { navigate } from "gatsby"
 
-const useFormValidation = initialState => {
+const useFormValidation = (initialState, validation) => {
+  // formの各値を保存
   const [values, setValues] = useState(initialState)
+  // formの各値がonChange eventで変更済がどうか、つまり一度でも入力されたかどうか
+  const [touched, setTouched] = useState({})
+  // touchedされた後、一度でもvalidatedされたかどうか
+  const [validated, setValidated] = useState({})
+  // validation errorsを格納
+  const [errors, setErrors] = useState({})
+  // onSubmitでloading trueにした後、useEffect hookでformを送信できるかどうかcheck
+  const [loading, setLoading] = useState(false)
+
+  // validation errorなし、かつ、すべてのform値がtouchedならform送信、loadingをfalseに
+  useEffect(() => {
+    if (loading) {
+      const noErrors = Object.keys(errors).length === 0
+      const allTouched = Object.keys(touched).length === 3
+      if (noErrors && allTouched) {
+        navigate("/contacts")
+        // 全stateを初期化
+        setValues(initialState)
+        setTouched({})
+        setValidated({})
+        setErrors({})
+        setLoading(false)
+      } else {
+        // errorありで、loadingだけfalseに
+        setLoading(false)
+      }
+    }
+  }, [touched, errors, loading])
 
   const handleChange = event => {
-    // eventはevent callbackが終わるとperformanceのためにnullにされるので非同期的にaccessしようとするとerrorとなるので、非同期でaccessしたい時はevent.persist()を使う
+    // eventはevent callbackが終わるとperformanceのためにnullにされるのでsetValuesのように非同期的にaccessしようとするとerrorとなるので、非同期でaccessする時はevent.persist()を使用
     event.persist()
-    setValues(previousValues => ({
-      ...previousValues,
+    setValues(prev => ({
+      ...prev,
       // ES6 Computed Property Name
       [event.target.name]: event.target.value,
     }))
+    setTouched(prev => ({
+      ...prev,
+      [event.target.name]: true,
+    }))
+  }
+
+  const handleBlur = event => {
+    event.persist()
+    const validationErrors = validation(values)
+    // ex. errors, touchedの構造
+    // errors = {
+    //   name: "sajskajskajskajk",
+    //   email: "sajksjaksjaksjak",
+    // }
+    // touched = {
+    //   email: true,
+    // }
+    // touched objectのkeyから配列を作成: Object.keys(touched)
+    // その配列からobjectを作成、初期値を{}にするのがpoint: .reduce((acc,cur,index)=>{}, {})
+    const errors = Object.keys(touched).reduce((acc, cur, index) => {
+      // loop中のkeyがerrorsになければ何もしない
+      if (!validationErrors[cur]) {
+        return acc
+      }
+      // loop中のkeyがtouchedされていれば、そのvalueを有効なerrorとして追加、untouchedならerrorとして追加しない
+      if (touched[cur]) {
+        acc[cur] = validationErrors[cur]
+        return acc
+      }
+    }, {})
+    setErrors(errors)
+    setValidatedWhenAllTouched(event)
   }
 
   const handleSubmit = event => {
+    event.persist()
     event.preventDefault()
-    console.log({ values })
+    const validationErrors = validation(values)
+    setErrors(validationErrors)
+    setValidatedWhenAllTouched(event)
+    setLoading(true)
   }
 
-  return { handleChange, handleSubmit, values }
+  const setValidatedWhenAllTouched = event => {
+    if (touched[event.target.name]) {
+      setValidated(prev => ({
+        ...prev,
+        [event.target.name]: true,
+      }))
+    }
+  }
+
+  return {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    values,
+    touched,
+    validated,
+    errors,
+    loading,
+  }
 }
 
 export default useFormValidation
