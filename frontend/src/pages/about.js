32b3bd4b10ea90f1d673a47022afde5eae93368a
Tatsuro <contacts@tatsuro.dev>
2021-02-03 21:23:59 +0900

    About fix

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { Row, Col } from "react-bootstrap"
+import styled from "styled-components"
 import Layout from "../components/Layout"
-import { ProgressBar } from "../components/common"
+import { Title, ProgressBar } from "../components/common"
 
 function About({ data }) {
   const {
@@ -12,34 +12,58 @@ function About({ data }) {
 
   return (
     <Layout>
-      <h1 className="pt-3">{title}</h1>
-      <hr />
-      <div>{content}</div>
-      <h1 className="pt-3">Skills</h1>
-      <hr />
-      <Row>
-        {skills.map(({ description, progress, tag: { name }, type }, index) => {
-          return (
-            <Col md={6} key={index}>
-              <div className="d-flex align-items-center">
-                <h3>{name}</h3>
-                <span className="pl-2">{description}</span>
-              </div>
-              <ProgressBar progress={progress} />
-            </Col>
-          )
-        })}
-        <Col className="pt-3 text-right">Updated At {updated_at}</Col>
-      </Row>
+      <StyledAboutPage className="container">
+        <Title title={title} />
+        <div>{content}</div>
+        <Title className="title" title="Skills" />
+        <div className="row">
+          {skills.map(
+            ({ description, progress, tag: { name }, type }, index) => {
+              return (
+                <div className="col-md-6" key={index}>
+                  <div className="skill-name">
+                    <h3>{name}</h3>
+                    <span>{description}</span>
+                  </div>
+                  <ProgressBar progress={progress} />
+                </div>
+              )
+            }
+          )}
+          {/* <div className="date">Updated At {updated_at}</div> */}
+        </div>
+      </StyledAboutPage>
     </Layout>
   )
 }
+
+const StyledAboutPage = styled.div`
+  padding: 2rem;
+
+  .title {
+    margin: 2rem 0;
+  }
+
+  .skill-name {
+    display: flex;
+    align-items: center;
+
+    span {
+      margin: 0 1rem;
+    }
+  }
+
+  .date {
+    float: right;
+  }
+`
+
 export const query = graphql`
   {
     strapiAbout {
       title
       content
-      updated_at(formatString: "YYYY, MM")
+      updated_at(formatString: "MMMM, YYYY")
     }
     allStrapiSkills(sort: { fields: order }) {
       nodes {
