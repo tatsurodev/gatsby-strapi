1df4c45d278c51efb1a418e725ae8ee1c653225f
Tatsuro <contacts@tatsuro.dev>
2021-02-03 18:09:43 +0900

    Footer fix

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,44 @@
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
+import styled from "styled-components"
+import { useLocation } from "@reach/router"
 
 const Footer = () => {
+  const isHome = useLocation().pathname === "/"
+
   return (
-    <footer>
-      <Container>
-        <Row>
-          <Col className="text-center py-3">Copyright &copy; tatsuro.dev</Col>
-        </Row>
-      </Container>
-    </footer>
+    <StyledFooter>
+      <div className={isHome ? "home" : ""}>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              Copyright {new Date().getFullYear()} &copy; tatsuro.dev
+            </div>
+          </div>
+        </div>
+      </div>
+    </StyledFooter>
   )
 }
+
+const StyledFooter = styled.footer`
+  padding: 1.2rem;
+  color: white;
+  text-align: center;
+  position: relative;
+  background: var(--info);
+
+  .home {
+    color: var(--gray);
+    ::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 30%;
+      right: 0;
+      bottom: 0;
+      background: white;
+    }
+  }
+`
 
 export default Footer
