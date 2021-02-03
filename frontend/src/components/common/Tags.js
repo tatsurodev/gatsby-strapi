dbf81378e1c9e0f3548ebd20fd13efce333dca3e
Tatsuro <contacts@tatsuro.dev>
2021-02-03 18:14:47 +0900

    Tags fix

--- frontend/src/components/common/Tags.js
+++ frontend/src/components/common/Tags.js
@@ -1,23 +1,49 @@
 import React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faTags } from "@fortawesome/free-solid-svg-icons"
+import { faTags, faHashtag } from "@fortawesome/free-solid-svg-icons"
 
 const Tags = ({ tags }) => {
   return (
-    <div className="d-flex align-items-center">
-      <FontAwesomeIcon icon={faTags} />
-      {tags.map((tag, index) => (
-        <Link
-          key={index}
-          to={`/tags/${tag}`}
-          className="btn btn-outline-info btn-sm ml-2"
-        >
-          {tag}
-        </Link>
-      ))}
-    </div>
+    <StyledTags>
+      <div className="row">
+        <div className="wrapper">
+          <div className="col-1">
+            <FontAwesomeIcon icon={faTags} />
+          </div>
+          <div className="col-11">
+            {tags.map((tag, index) => (
+              <Link
+                key={index}
+                to={`/tags/${tag}`}
+                className="btn btn-outline-info btn-sm"
+              >
+                <FontAwesomeIcon icon={faHashtag} />
+                {tag}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </StyledTags>
   )
 }
+
+const StyledTags = styled.div`
+  .wrapper {
+    display: flex;
+    align-items: center;
+    /* height: 6rem;
+
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    overflow: hidden; */
+  }
+  a {
+    margin: 0.3rem;
+  }
+`
 
 export { Tags }
