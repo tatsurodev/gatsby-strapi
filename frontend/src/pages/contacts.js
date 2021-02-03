db5f429e5054da7aab67385f7da927c8386da63c
Tatsuro <contacts@tatsuro.dev>
2021-02-03 22:28:02 +0900

    Contacts fix

--- frontend/src/pages/contacts.js
+++ frontend/src/pages/contacts.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 import useContactValidation from "../hooks/useContactValidation"
 import { contactValidation } from "../utils"
 import Layout from "../components/Layout"
@@ -33,65 +34,71 @@ const ContactsPage = () => {
 
   return (
     <Layout>
-      <Message />
-      <Title title="Contact Me" />
-      <form
-        onSubmit={handleSubmit}
-        name="contact"
-        method="post"
-        data-netlify="true"
-        netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="contact" />
-        <Input
-          tag="input"
-          type="text"
-          name="name"
-          values={values}
-          icon={faUser}
-          placeholder="Name"
-          errors={errors}
-          touched={touched}
-          validated={validated}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-        <Input
-          tag="input"
-          type="email"
-          name="email"
-          values={values}
-          icon={faEnvelope}
-          placeholder="Email"
-          errors={errors}
-          touched={touched}
-          validated={validated}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-        <Input
-          tag="textarea"
-          name="message"
-          values={values}
-          icon={faPencilAlt}
-          placeholder="Message"
-          errors={errors}
-          touched={touched}
-          validated={validated}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-        <button
-          type="submit"
-          className="btn btn-info btn-block btn-lg"
-          disabled={loading}
+      <StyledContacts className="container">
+        <Message />
+        <Title title="Contact Me" />
+        <form
+          onSubmit={handleSubmit}
+          name="contact"
+          method="post"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
         >
-          Submit
-        </button>
-      </form>
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contact" />
+          <Input
+            tag="input"
+            type="text"
+            name="name"
+            values={values}
+            icon={faUser}
+            placeholder="Name"
+            errors={errors}
+            touched={touched}
+            validated={validated}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <Input
+            tag="input"
+            type="email"
+            name="email"
+            values={values}
+            icon={faEnvelope}
+            placeholder="Email"
+            errors={errors}
+            touched={touched}
+            validated={validated}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <Input
+            tag="textarea"
+            name="message"
+            values={values}
+            icon={faPencilAlt}
+            placeholder="Message"
+            errors={errors}
+            touched={touched}
+            validated={validated}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <button
+            type="submit"
+            className="btn btn-info btn-block btn-lg"
+            disabled={loading}
+          >
+            Submit
+          </button>
+        </form>
+      </StyledContacts>
     </Layout>
   )
 }
+
+const StyledContacts = styled.div`
+  padding: 2rem;
+`
 
 export default ContactsPage
