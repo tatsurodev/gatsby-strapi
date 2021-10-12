1a0a43184d29d472138facbcf3c26e11551549e2
Tatsuro <contacts@tatsuro.dev>
2021-10-13 05:00:31 +0900

    gatsbyとstrapi接続

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -36,5 +36,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: process.env.GATSBY_STRAPI_API_URL,
+        collectionTypes: [],
+        singleTypes: [],
+        queryLimit: 1000,
+      },
+    },
   ],
 }
