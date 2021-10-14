5853e078d82d3e729f105abbb667a5952002b917
Tatsuro <contacts@tatsuro.dev>
2021-10-15 03:29:07 +0900

    Title作成

---
+++ frontend/src/components/common/Title.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Breadcrumb } from '.'
+import styled from 'styled-components'
+
+const Title = ({ title, pageContext }) => {
+  return (
+    <StyledTitle>
+      <h1>{title}</h1>
+      <Breadcrumb pageContext={pageContext} crumbLabel={title} />
+    </StyledTitle>
+  )
+}
+
+const StyledTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    font-size: 2.4rem;
+    text-transform: capitalize;
+    margin-bottom: 0.5rem;
+  }
+`
+export { Title }
