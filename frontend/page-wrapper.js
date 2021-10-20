22e3a7a058bb1a1565bdc240f78ab460af97b0ed
Tatsuro <contacts@tatsuro.dev>
2021-10-21 02:45:36 +0900

    MDXRendererでmdx内のtagを書き換える

--- frontend/page-wrapper.js
+++ frontend/page-wrapper.js
@@ -2,13 +2,26 @@ import React from 'react'
 import GlobalStyle from './src/css/GlobalStyle'
 import { Layout } from './src/components/layout'
 import 'bootswatch/dist/pulse/bootstrap.min.css'
+import { MDXProvider } from '@mdx-js/react'
+import { H1, H2, H3, Code, Notice, Prism } from './src/components/blogs'
+
+const components = {
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  inlineCode: Code,
+  section: Notice,
+  pre: Prism,
+}
 
 // app全体のuiに関するような処理をwrapPageElementに記述
 export const wrapPageElement = ({ element, props }) => {
   return (
     <>
       <GlobalStyle />
-      <Layout {...props}>{element}</Layout>
+      <Layout {...props}>
+        <MDXProvider components={components}>{element}</MDXProvider>
+      </Layout>
     </>
   )
 }
