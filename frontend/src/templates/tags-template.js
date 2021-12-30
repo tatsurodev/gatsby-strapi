643e1a450e455491f6169fd3fc4b10a0c27de8b5
Tatsuro <contacts@tatsuro.dev>
2021-12-30 14:32:56 +0900

    tags template作成

---
+++ frontend/src/templates/tags-template.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Title, Articles } from '../components/common'
+import useFilter from '../hooks/useFilter'
+
+const TagsTemplate = ({ pageContext }) => {
+  const { tag, portfolios, reviews, blogs } = pageContext
+  const { articles: filteredPortfolios } = useFilter(portfolios)
+  const { articles: filteredReviews } = useFilter(reviews)
+  const { articles: filteredBlogs } = useFilter(blogs)
+
+  return (
+    <StyledTagsTemplate>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Title title={tag.name} pageContext={pageContext} />
+          </div>
+        </div>
+        <Articles articles={filteredPortfolios} prefix="portfolios" />
+        <Articles articles={filteredReviews} prefix="reviews" />
+        <Articles articles={filteredBlogs} prefix="blogs" />
+      </div>
+    </StyledTagsTemplate>
+  )
+}
+
+const StyledTagsTemplate = styled.div`
+  padding: 2rem;
+`
+
+export default TagsTemplate
