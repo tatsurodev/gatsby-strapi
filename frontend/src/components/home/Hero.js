7ccd2d61abc4faca3448577b66d5c268a2f46adb
Tatsuro <contacts@tatsuro.dev>
2022-03-09 07:24:52 +0900

    Hero fix

--- frontend/src/components/home/Hero.js
+++ frontend/src/components/home/Hero.js
@@ -2,42 +2,38 @@ import React from 'react'
 import styled, { keyframes } from 'styled-components'
 import { StaticImage } from 'gatsby-plugin-image'
 import { FaCheckSquare } from 'react-icons/fa'
+import { hero } from '../../constants/hero'
 
 const Hero = () => {
+  const { leading, subleading, skills } = hero
+
   return (
     <StyledHero>
-      <div className="heading">
-        <h1 className="leading">I'm Tatsuro</h1>
-        <div className="sub-leading">
-          <div>I love coding with</div>
-          <div className="skill">
-            <FaCheckSquare className="icon" /> Laravel
-          </div>
-          <div className="skill">
-            <FaCheckSquare className="icon" /> Vue
-          </div>
-          <div className="skill">
-            <FaCheckSquare className="icon" /> React
-          </div>
-          <div className="skill">
-            <FaCheckSquare className="icon" /> Docker
-          </div>
-        </div>
-      </div>
-      <div className="image">
-        <div className="hero-wrapper">
+      <StyledHeadings>
+        <h1>{leading}</h1>
+        <StyledSkills>
+          <p>{subleading}</p>
+          {skills.map(skill => (
+            <StyledSkill key={skill}>
+              <FaCheckSquare /> {skill}
+            </StyledSkill>
+          ))}
+        </StyledSkills>
+      </StyledHeadings>
+      <StyledImageWrapper>
+        <StyledImage>
           <StaticImage
             src="../../images/avatar.png"
             alt="avatar"
             layout="fullWidth"
             placeholder="blurred"
-            className="hero"
+            className="hero__image"
           />
-          <div className="bg-circle1"></div>
-          <div className="bg-circle2"></div>
-          <div className="bg-circle3"></div>
-        </div>
-      </div>
+          <StyledImageBgCircle1 />
+          <StyledImageBgCircle2 />
+          <StyledImageBgCircle3 />
+        </StyledImage>
+      </StyledImageWrapper>
     </StyledHero>
   )
 }
@@ -55,87 +51,91 @@ const StyledHero = styled.div`
   height: 100vh;
   position: relative;
 
-  ::before {
+  &::before {
     content: '';
     position: absolute;
     top: 0;
     left: 30%;
     right: 0;
     bottom: 0;
-    background: var(--bs-primary);
+    background: ${({ theme }) => theme.secondaryBg};
   }
+`
 
-  .heading {
-    color: white;
-    position: absolute;
-    top: 50%;
-    left: 70%;
-    transform: translate(-50%, -50%);
+const StyledHeadings = styled.div`
+  color: ${({ theme }) => theme.headings};
+  position: absolute;
+  top: 50%;
+  left: 70%;
+  transform: translate(-50%, -50%);
 
-    .leading {
-      font-size: 5vw;
-    }
-
-    .sub-leading {
-      padding: 0 2rem;
-      font-size: 1.5vw;
-
-      .skill {
-        display: flex;
-        align-items: center;
-      }
-
-      .icon {
-        margin: 0 0.2rem;
-      }
-
-      > div {
-        margin: 1rem 0;
-      }
-    }
+  > h1 {
+    font-size: 5vw;
   }
+`
 
-  .image {
+const StyledSkills = styled.div`
+  padding: 0 2rem;
+  font-size: 1.5vw;
+
+  > p {
+    margin: 1rem 0;
+  }
+`
+
+const StyledSkill = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 1rem 0;
+
+  > svg {
+    margin: 0 0.2rem;
+  }
+`
+
+const StyledImageWrapper = styled.div`
+  height: 25vw;
+  width: 25vw;
+  position: absolute;
+  left: 30%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const StyledImage = styled.div`
+  position: relative;
+
+  > div {
     height: 25vw;
     width: 25vw;
-    position: absolute;
-    left: 30%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-
-    .hero-wrapper {
-      position: relative;
-
-      > div {
-        height: 25vw;
-        width: 25vw;
-        position: absolute !important;
-      }
-
-      .hero {
-        z-index: 100;
-        border-radius: 50% 50% 50% 70%/50% 50% 70% 60%;
-      }
-      .bg-circle1 {
-        opacity: 0.6;
-        background: var(--bs-primary);
-        border-radius: 50% 50% 50% 70%/50% 50% 70% 60%;
-        animation: ${circleRotate} 6s infinite linear;
-      }
-      .bg-circle2 {
-        opacity: 0.4;
-        background: var(--bs-light);
-        border-radius: 80% 30% 50% 50%/50%;
-        animation: ${circleRotate} 12s infinite linear;
-      }
-      .bg-circle3 {
-        opacity: 0.2;
-        background: var(--bs-primary);
-        border-radius: 40% 40% 50% 40%/30% 50% 50% 50%;
-        animation: ${circleRotate} 18s infinite linear;
-      }
-    }
+    position: absolute !important;
   }
+
+  > .hero__image {
+    z-index: 100;
+    border-radius: 50% 50% 50% 70%/50% 50% 70% 60%;
+  }
+`
+
+const StyledImageBgCircle1 = styled.div`
+  opacity: 0.6;
+  background: ${({ theme }) => theme.oppositePrimaryBg};
+  border-radius: 50% 50% 50% 70%/50% 50% 70% 60%;
+  animation: ${circleRotate} 6s infinite linear;
+`
+
+const StyledImageBgCircle2 = styled.div`
+  opacity: 0.4;
+  background: ${({ theme }) => theme.primaryBg};
+  border-radius: 80% 30% 50% 50%/50%;
+  animation: ${circleRotate} 12s infinite linear;
+`
+
+const StyledImageBgCircle3 = styled.div`
+  opacity: 0.2;
+  background: ${({ theme }) => theme.oppositePrimaryBg};
+  border-radius: 40% 40% 50% 40%/30% 50% 50% 50%;
+  animation: ${circleRotate} 18s infinite linear;
 `
 
 export { Hero }
---
+++ frontend/src/constants/hero.js
@@ -0,0 +1,7 @@
+const hero = {
+  leading: "I'm Tatsuro",
+  subleading: 'I love coding with',
+  skills: ['React', 'Gatsyby', 'Laravel', 'Vue', 'Docker'],
+}
+
+export { hero }
