299af015c78c9fdcd7049bb579cb0947b60a2081
Tatsuro <contacts@tatsuro.dev>
2022-03-09 07:25:41 +0900

    Tags fix

--- frontend/src/components/common/Tags.js
+++ frontend/src/components/common/Tags.js
@@ -6,21 +6,17 @@ import { FaTags, FaHashtag } from 'react-icons/fa'
 const Tags = ({ tags }) => {
   return (
     <StyledTags>
-      <div>
+      <StyledIcon>
         <FaTags />
-      </div>
-      <div className="tags">
+      </StyledIcon>
+      <StyledTagsWrapper>
         {tags.map((tag, index) => (
-          <Link
-            key={index}
-            to={`/tags/${tag.toLowerCase()}`}
-            className="btn btn-outline-dark btn-sm tag"
-          >
-            <FaHashtag className="hash" />
+          <StyledLink key={index} to={`/tags/${tag.toLowerCase()}`}>
+            <FaHashtag />
             <span>{tag}</span>
-          </Link>
+          </StyledLink>
         ))}
-      </div>
+      </StyledTagsWrapper>
     </StyledTags>
   )
 }
@@ -29,21 +25,37 @@ const StyledTags = styled.div`
   margin-top: 1rem;
   display: flex;
   align-items: center;
+`
 
-  .tags {
-    margin-left: 0.5rem;
-    flex-wrap: wrap;
-    > .tag {
-      margin: 0.25rem;
-    }
-  }
-
-  .hash {
-    margin-right: 0.1rem;
-  }
-
-  svg {
+const StyledIcon = styled.div`
+  > svg {
     vertical-align: middle;
+  }
+`
+
+const StyledTagsWrapper = styled.div`
+  margin-left: 0.5rem;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  margin: 0.25rem;
+  padding: 0.2rem 0.4rem;
+  border-radius: 0.8rem;
+  border: 1px solid rgba(0, 0, 0, 0);
+  background: ${({ theme }) => theme.btnPrimaryBg};
+  color: ${({ theme }) => theme.btnPrimaryText};
+
+  &:hover {
+    background: ${({ theme }) => theme.btnPrimaryBgHover};
+    color: ${({ theme }) => theme.btnPrimaryTextHover};
+  }
+
+  > svg {
+    margin-right: 0.1rem;
   }
 `
 
