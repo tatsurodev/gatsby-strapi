ed68ab0013a70055c86bb11b249f441917dfe4de
Tatsuro <contacts@tatsuro.dev>
2022-03-09 07:32:03 +0900

    TocHeadings fix

--- frontend/src/components/blogs/TocHeadings.js
+++ frontend/src/components/blogs/TocHeadings.js
@@ -5,53 +5,49 @@ import scrollTo from 'gatsby-plugin-smoothscroll'
 const TocHeadings = ({ items, parentIndex }) => {
   return (
     <StyledTocHeadings>
-      <ul>
-        {items.map((item, index) => {
-          const currentIndex = parentIndex
-            ? `${parentIndex}.${index + 1}`
-            : `${index + 1}`
-          return (
-            <li key={item.url}>
-              <a
-                href={item.url}
-                key={item.url}
-                onClick={() => scrollTo(`[href='${item.url}']`)}
-              >
-                <span className="prefix">
-                  {parentIndex ? `${currentIndex}` : index + 1}
-                </span>
-                <span className="title">{item.title}</span>
-              </a>
-              {item.items && (
-                <TocHeadings items={item.items} parentIndex={currentIndex} />
-              )}
-            </li>
-          )
-        })}
-      </ul>
+      {items.map((item, index) => {
+        const currentIndex = parentIndex
+          ? `${parentIndex}.${index + 1}`
+          : `${index + 1}`
+        return (
+          <StyledLi key={item.url}>
+            <a
+              href={item.url}
+              key={item.url}
+              onClick={() => scrollTo(`[href='${item.url}']`)}
+            >
+              <span>{parentIndex ? `${currentIndex}` : index + 1}</span>
+              <StyledSpan>{item.title}</StyledSpan>
+            </a>
+            {item.items && (
+              <TocHeadings items={item.items} parentIndex={currentIndex} />
+            )}
+          </StyledLi>
+        )
+      })}
     </StyledTocHeadings>
   )
 }
 
-const StyledTocHeadings = styled.div`
-  ul,
-  li {
-    font-size: 1rem;
-    margin: 0.4rem 0 0 0.4rem;
-    list-style: none;
-  }
-  a {
+const StyledTocHeadings = styled.ul``
+
+const StyledLi = styled.li`
+  font-size: 1rem;
+  margin: 0.4rem 0 0 0.4rem;
+  list-style: none;
+
+  > a {
     display: block;
-    color: var(--gray);
+    color: ${({ theme }) => theme.links};
+
+    &:hover {
+      opacity: 0.8;
+    }
   }
-  a:hover {
-    opacity: 0.8;
-  }
-  .prefix {
-  }
-  .title {
-    margin-left: 0.4rem;
-  }
+`
+
+const StyledSpan = styled.span`
+  margin-left: 0.4rem;
 `
 
 export { TocHeadings }
