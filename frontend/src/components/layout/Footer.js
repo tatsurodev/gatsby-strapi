902aad29566af5328ae551bd061696d7e8eaec7d
Tatsuro <contacts@tatsuro.dev>
2022-03-09 07:22:57 +0900

    Footer fix

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,42 +1,52 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { useLocation } from '@reach/router'
-import { Container, Row, Col } from 'react-bootstrap'
+
 const Footer = () => {
   const isHome = useLocation().pathname === '/'
 
   return (
-    <StyledFooter>
-      <div className={isHome ? 'home' : ''}>
-        <Container>
-          <Row>
-            <Col>Copyright {new Date().getFullYear()} &copy; tatsuro.dev</Col>
-          </Row>
-        </Container>
+    <StyledFooter isHome={isHome}>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <span>Copyright {new Date().getFullYear()} &copy; tatsuro.dev</span>
+          </div>
+        </div>
       </div>
     </StyledFooter>
   )
 }
 
-const StyledFooter = styled.footer`
-  padding: 1.2rem;
-  color: var(--bs-white);
-  text-align: center;
-  position: relative;
-  background: var(--bs-primary);
-
-  .home {
-    color: var(--bs-gray);
-    ::before {
-      content: '';
-      position: absolute;
-      top: 0;
-      left: 30%;
-      right: 0;
-      bottom: 0;
-      background: var(--bs-white);
-    }
+const homeStyle = ({ isHome }) => {
+  if (isHome) {
+    return css`
+      &::before {
+        content: '';
+        background: ${({ theme }) => theme.secondaryBg};
+        position: absolute;
+        top: 0;
+        left: 0%;
+        right: 70%;
+        bottom: 0;
+      }
+    `
   }
+}
+
+const StyledFooter = styled.footer`
+  position: relative;
+  padding: 1.2rem;
+  color: ${({ theme }) => theme.text};
+  text-align: center;
+  // homeだけbg変更
+  background: ${({ theme, isHome }) =>
+    isHome ? theme.primaryBg : theme.secondaryBg};
+  // homeだけborder-topなし
+  border-top: ${({ theme, isHome }) =>
+    isHome ? 'none' : `1px solid ${theme.borderColor}`};
+
+  ${homeStyle}
 `
 
 export { Footer }
