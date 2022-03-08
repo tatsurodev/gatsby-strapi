57934396470279771b1e8eba788ffda530713e7c
Tatsuro <contacts@tatsuro.dev>
2022-03-09 07:26:54 +0900

    rating fix

--- frontend/src/components/common/Rating.js
+++ frontend/src/components/common/Rating.js
@@ -1,43 +1,47 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FaStar, FaRegStar } from 'react-icons/fa'
+import { H1 } from '../blogs'
 
-const Rating = ({ children, className, rating, reviewTitle }) => {
+const Rating = ({ children, rating, reviewTitle }) => {
   return (
-    <StyledRating className={className}>
-      <h3 className="heading">{`レビュー星${rating}つ`}</h3>
+    <StyledRating>
+      <H1 center fontSize="1.2rem">
+        {reviewTitle}
+      </H1>
       {children}
-      <div className="stars">
+      <StyledStars>
         {rating >= 1 ? <FaStar /> : <FaRegStar />}
         {rating >= 2 ? <FaStar /> : <FaRegStar />}
         {rating >= 3 ? <FaStar /> : <FaRegStar />}
         {rating >= 4 ? <FaStar /> : <FaRegStar />}
         {rating >= 5 ? <FaStar /> : <FaRegStar />}
-      </div>
-      <h3 className="heading">{reviewTitle}</h3>
+      </StyledStars>
     </StyledRating>
   )
 }
 
+// styled componentは他のstyled component内で参照できる。H1はreact componentなので参照できない
 const StyledRating = styled.div`
-  .heading {
-    display: flex;
-    justify-content: center;
-    margin-bottom: 2rem;
-    padding: 0.5rem;
-    color: var(--bs-white);
-    background: var(--bs-primary);
-    border-radius: 10px;
-  }
+  // don't work
+  // ${H1} {
+  //   text-align: center;
+  // }
+`
 
-  .stars {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 2rem 0;
-    font-size: 1.5rem;
-    color: var(--bs-warning);
-  }
+// styled componentの拡張
+// const StyledH1 = StyledComponentName.extend``
+
+// react componentの拡張、参照されるreact componentはpropsにclassNameを追加する必要あり
+// const StyledH1 = styled(H1)``
+
+const StyledStars = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem 0;
+  font-size: 1.5rem;
+  color: var(--bs-warning);
 `
 
 export { Rating }
