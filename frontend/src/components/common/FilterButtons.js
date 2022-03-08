f912681069774e15cb01442a5afebe3650a19cb3
Tatsuro <contacts@tatsuro.dev>
2022-03-09 07:27:49 +0900

    FilterButtons fix

--- frontend/src/components/common/FilterButtons.js
+++ frontend/src/components/common/FilterButtons.js
@@ -2,27 +2,21 @@ import React from 'react'
 import styled from 'styled-components'
 import { FaMinusCircle, FaHashtag } from 'react-icons/fa'
 
-const FilterButtons = ({
-  tags,
-  selectedTags,
-  handleChange,
-  resetArticles,
-  className,
-}) => {
+const FilterButtons = ({ tags, selectedTags, handleChange, resetArticles }) => {
   return (
-    <StyledFilterButtons className={className}>
-      <button
-        className="btn btn-danger tag reset"
+    <StyledFilterButtons>
+      <StyledButton
+        reset
         onClick={resetArticles}
         disabled={selectedTags.length === 0}
       >
         <FaMinusCircle />
-        <span>Reset Filters</span>
-      </button>
-      <form className="tags">
+        <span>reset filters</span>
+      </StyledButton>
+      <StyledForm>
         {tags.map((tag, index) => (
           <div key={index}>
-            <input
+            <StyledInput
               type="checkbox"
               name="filters[]"
               value={tag}
@@ -31,13 +25,13 @@ const FilterButtons = ({
               onChange={handleChange}
               checked={selectedTags.includes(tag)}
             />
-            <label htmlFor={`checkbox-${tag}`} className="tag">
+            <StyledLabel htmlFor={`checkbox-${tag}`}>
               <FaHashtag />
               <span>{tag}</span>
-            </label>
+            </StyledLabel>
           </div>
         ))}
-      </form>
+      </StyledForm>
     </StyledFilterButtons>
   )
 }
@@ -45,57 +39,54 @@ const FilterButtons = ({
 const StyledFilterButtons = styled.div`
   text-align: center;
   padding: 2rem;
-  background: var(--bs-light);
+  background: ${({ theme }) => theme.secondaryBg};
   border-radius: 5px;
+`
 
-  input[type='checkbox'] {
-    display: none;
+const StyledForm = styled.form`
+  display: flex;
+  justify-content: center;
+`
+
+const StyledInput = styled.input`
+  display: none;
+
+  &:checked + label {
+    background: ${({ theme }) => theme.btnPrimaryBgHover};
+    color: ${({ theme }) => theme.btnPrimaryTextHover};
+  }
+`
+
+const StyledButton = styled.button`
+  margin: 0.25rem 0.25rem ${({ reset }) => (reset ? '2rem' : '0.25rem')} 0.25rem;
+  padding: 0.2rem 0.4rem;
+  border-radius: 0.8rem;
+  cursor: pointer;
+  border: 1px solid rgba(0, 0, 0, 0);
+  background: ${({ theme, reset }) =>
+    reset ? theme.btnDangerBg : theme.btnPrimaryBg};
+  color: ${({ theme, reset }) => (reset ? theme.text : theme.btnPrimaryText)};
+
+  > svg {
+    margin-right: 0.2rem;
+    vertical-align: text-bottom;
   }
 
-  .tags {
-    display: flex;
-    justify-content: center;
-  }
-
-  .tag {
-    margin: 5px;
-    padding: 0.25rem 0.5rem;
-    height: 2rem;
-    border: 2px solid var(--bs-primary);
-    cursor: pointer;
-    color: var(--bs-primary);
-    font-size: 0.875rem;
-    line-height: 1.5;
-    font-weight: 700;
-
-    svg {
-      margin-right: 0.2rem;
-      vertical-align: middle;
-    }
-  }
-
-  .reset {
-    color: var(--bs-white);
-    border: initial;
-    margin-bottom: 1rem;
-
-    :disabled {
-      opacity: 0.3;
-    }
-  }
-
-  input[type='checkbox']:checked + label {
-    color: white;
-    background: var(--bs-primary);
-  }
-
-  label:hover {
-    color: white;
-    background: var(--bs-primary);
-    border-color: var(--bs-primary);
+  &:hover {
+    color: ${({ theme, reset }) =>
+      reset ? theme.text : theme.btnPrimaryTextHover};
+    background: ${({ theme, reset }) =>
+      reset ? theme.btnDangerBg : theme.btnPrimaryBgHover};
+    border-color: ${({ theme }) => theme.borderColor};
     transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
       border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
   }
+
+  &:disabled {
+    opacity: 0.3;
+  }
 `
+
+const StyledLabel = StyledButton.withComponent('label')
 
 export { FilterButtons }
