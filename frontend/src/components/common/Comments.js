378e5992fd941643ff67c18e9b0d7919da3ad7b9
Tatsuro <contacts@tatsuro.dev>
2022-03-09 07:28:21 +0900

    comments fix

--- frontend/src/components/common/Comments.js
+++ frontend/src/components/common/Comments.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Disqus } from 'gatsby-plugin-disqus'
 
-const Comments = ({ id, title, url, className }) => {
+const Comments = ({ id, title, url }) => {
   const config = {
     identifier: id,
     title,
@@ -10,22 +10,23 @@ const Comments = ({ id, title, url, className }) => {
   }
 
   return (
-    <StyledComments>
+    <>
       {process.env.GATSBY_DISQUS_SHORTNAME && (
-        <Disqus config={config} className={className} />
+        <StyledComments>
+          <Disqus config={config} />
+        </StyledComments>
       )}
-    </StyledComments>
+    </>
   )
 }
 
 const StyledComments = styled.div`
-   {
-    margin: 2rem 0;
-    padding: 1rem;
-    border-radius: 10px;
-    border: 1px solid var(--bs-gray-200);
-    background: var(--bs-white);
-  }
+  color: ${({ theme }) => theme.text};
+  margin: 2rem 0;
+  padding: 1rem;
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.borderColor};
+  background: ${({ theme }) => theme.primaryBg};
 `
 
 export { Comments }
